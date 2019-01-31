575aa9a255067783265369ced61efc2aad271277
Vitaly _Vi Shukela <vi0oss@gmail.com>
2019-01-31 03:56:34 +0300

    A STUN sample

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,28 @@ extern crate rustun;
 extern crate structopt;
 extern crate futures;
 extern crate tokio;
+extern crate fibers;
+extern crate fibers_transport;
 
 
 use std::net::SocketAddr;
 use structopt::StructOpt;
 
 
+use fibers_transport::UdpTransporter;
 use futures::{Async, Future, Poll};
 use rusturn::auth::AuthParams;
-use rusturn::client::{wait, Client, UdpClient};
+use rusturn::client::{wait, Client as TurnClient, UdpClient as TurnUdpClient};
 use rusturn::Error;
+use fibers::{Executor,Spawn};
+
+use rustun::channel::Channel;
+use rustun::client::{Client as StunClient};
+use rustun::message::Request;
+use rustun::transport::StunUdpTransporter;
+use std::net::ToSocketAddrs;
+use stun_codec::rfc5389;
+use stun_codec::{MessageDecoder, MessageEncoder};
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -35,18 +47,40 @@ struct Opt {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Opt::from_args();
 
-    let mut rt = tokio::runtime::current_thread::Runtime::new()?;
+    //let mut rt = tokio::runtime::current_thread::Runtime::new()?;
+    let mut rt = fibers::executor::InPlaceExecutor::new()?;
+    let h = rt.handle();
 
     let auth_params = AuthParams::new(&opt.username, &opt.password)?;
 
-    let client = UdpClient::allocate(
+    let local_addr = "0.0.0.0:0".parse().unwrap();
+    let response = UdpTransporter::<MessageEncoder<_>, MessageDecoder<_>>::bind(local_addr)
+        .map_err(Error::from)
+        .map(StunUdpTransporter::new)
+        .map(Channel::new)
+        .and_then(move |channel| {
+            let h = h;
+            let client = StunClient::new(&h, channel);
+            let request = Request::<rfc5389::Attribute>::new(rfc5389::methods::BINDING);
+            client.call(opt.server, request)
+        });
+    let monitor = rt.spawn_monitor(response);
+    let response = rt.run_fiber(monitor)??.map_err(|e|format!("{:?}",e))?;
+    let addr : &rfc5389::attributes::XorMappedAddress = response.get_attribute().ok_or_else(||format!("No XorMappedAddr?"))?;
+    let addr = addr.address();
+    eprintln!("STUN response: {:?}", addr);
+
+
+    /*
+    let client = TurnUdpClient::allocate(
         opt.server,
         auth_params
     );
-
-    let client = rt.block_on(client)?;
+    let mut monitor = rt.spawn_monitor(client);
+    let client = rt.run_fiber(monitor)??;
 
     eprintln!("{:?}", client.relay_addr());
+    */
 
     Ok(())
 }
