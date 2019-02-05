9cf5be44de4c476418c2be11abeecc5c9f103609
Vitaly _Vi Shukela <vi0oss@gmail.com>
2019-02-05 16:02:35 +0300

    Pre-cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,28 @@
 #![allow(unused)]
 
-
-extern crate rusturn;
-extern crate rustun;
-extern crate structopt;
-extern crate futures;
-extern crate tokio;
+extern crate bytecodec;
 extern crate fibers;
 extern crate fibers_transport;
-extern crate bytecodec;
-
+extern crate futures;
+extern crate rand;
+extern crate rustun;
+extern crate rusturn;
+extern crate structopt;
+extern crate stunclient;
+extern crate tokio;
 
 use std::net::SocketAddr;
 use structopt::StructOpt;
 
-
+use fibers::{Executor, Spawn};
 use fibers_transport::UdpTransporter;
 use futures::{Async, Future, Poll};
 use rusturn::auth::AuthParams;
 use rusturn::client::{wait, Client as TurnClient, UdpClient as TurnUdpClient};
 use rusturn::Error;
-use fibers::{Executor,Spawn};
 
 use rustun::channel::Channel;
-use rustun::client::{Client as StunClient};
+use rustun::client::Client as StunClient;
 use rustun::message::Request;
 use rustun::transport::StunUdpTransporter;
 use std::net::ToSocketAddrs;
@@ -31,10 +30,11 @@ use stun_codec::rfc5389;
 use stun_codec::{MessageDecoder, MessageEncoder};
 
 use bytecodec::{DecodeExt, EncodeExt};
-use stun_codec::{Message, MessageClass, TransactionId};
+use stun_codec::rfc5389::attributes::{
+    MappedAddress, Software, XorMappedAddress, XorMappedAddress2,
+};
 use stun_codec::rfc5389::{methods::BINDING, Attribute};
-use stun_codec::rfc5389::attributes::{Software,XorMappedAddress};
-
+use stun_codec::{Message, MessageClass, TransactionId};
 
 #[derive(Debug, StructOpt)]
 struct Opt {
@@ -54,37 +54,13 @@ struct Opt {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let opt = Opt::from_args();
 
-    let local_addr : SocketAddr = "0.0.0.0:0".parse().unwrap();
+    let local_addr: SocketAddr = "0.0.0.0:0".parse().unwrap();
     let udp = std::net::UdpSocket::bind(local_addr)?;
 
-    
-    let mut message = Message::new(
-        MessageClass::Request,
-        BINDING,
-        TransactionId::new([3; 12])
-    );
-    message.add_attribute(Attribute::Software(Software::new("TURN_Hammer".to_owned())?));
-
-    // Encodes the message
-    let mut encoder = MessageEncoder::new();
-    let bytes = encoder.encode_into_bytes(message.clone())?;
-
-    udp.send_to(&bytes[..], opt.server)?;
-
-    let mut buf = [0; 1024];
-    let (len, addr) = udp.recv_from(&mut buf[..])?;
-    let buf = &buf[0..len];
-
-    //eprintln!("Received reply from {:?}", addr);
-
-    let mut decoder = MessageDecoder::<Attribute>::new();
-    let decoded = decoder.decode_from_bytes(buf)?.map_err(|_|format!("Broken STUN reply"))?;
-    
-    //eprintln!("Decoded message: {:?}", decoded);
-    let external_addr : &XorMappedAddress = decoded.get_attribute().ok_or_else(||format!("No XorMappedAddress?"))?;
-    let external_addr = external_addr.address();
-
-    println!("{}", external_addr);
+    let extaddr = stunclient::StunClient::new(opt.server)
+        .set_software(Some("TURN_hammer"))
+        .query_external_address(&udp)?;
+    eprintln!("External address: {}", extaddr);
 
     //let mut rt = tokio::runtime::current_thread::Runtime::new()?;
     /*
@@ -110,8 +86,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = addr.address();
     eprintln!("STUN response: {:?}", addr);
     */
-
-
 
     /*
     let client = TurnUdpClient::allocate(
